79e8ce0bdbd619bac7beea132565404a692d93a0
mishraaman435 <89015496+mishraaman435@users.noreply.github.com>
2026-04-05 19:38:23 +0530

    Update API URL for production environment
    Replaced production API URL with localhost for testing.

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,8 +1,6 @@
 
 export const environment = {
   production: true,
-  // apiUrl: 'https://localhost:7107/api',
-  // apiUrl: 'https://103.94.204.143/FetchUtility_api/api',
-  apiUrl: 'https://gisuat.mp.gov.in/FetchUtility_api/api',
-
+   apiUrl: 'https://localhost:7107/api',
+ 
 };
