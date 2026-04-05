fc72f62122f0ef84e99fa6027d684dd0579df4a5
mishraaman435 <89015496+mishraaman435@users.noreply.github.com>
2026-04-05 19:39:02 +0530

    Change apiUrl to new production endpoint
    Updated the apiUrl to point to the new production endpoint.

--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -4,12 +4,7 @@
 
 export const environment = {
   production: false,
-
-  // apiUrl: 'https://103.94.204.143/FetchUtility_api/api',
-  apiUrl: 'https://gisuat.mp.gov.in/FetchUtility_api/api',
-  // apiUrl: 'http://[::1]/api',
   // apiUrl: 'https://localhost:7107/api',
-  // apiUrl:'https://192.168.149.243/api',
 };
 
 /*F
