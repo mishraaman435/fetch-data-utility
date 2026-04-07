f0087019508fea2501a2dbc43a873eca04a6798e
mishraaman435 <89015496+mishraaman435@users.noreply.github.com>
2026-04-08 00:27:18 +0530

    Add files via upload

---
+++ Backend/BAL/LoginManager.cs
@@ -0,0 +1,47 @@
+﻿using DAL;
+using Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAL
+{
+
+    public class LoginManager : IDisposable
+    {
+        private IConfig _config;
+        public LoginManager(IConfig config)
+        {
+            _config = config;
+        }
+
+        public void Dispose()
+        {
+            // throw new NotImplementedException();
+        }
+
+        public async Task<dynamic> Login(LoginEntity webLogin)
+        {
+
+            using (LoginHelper db = new LoginHelper(_config))
+            {
+                return await db.Login(webLogin);
+            }
+
+        }
+        public async Task<dynamic> RegisterUser(RegisterEntity webLogin)
+        {
+
+            using (LoginHelper db = new LoginHelper(_config))
+            {
+                
+                return await db.RegisterUser(webLogin);
+            }
+            return 1;
+
+        }
+
+    }
+}
---
+++ Backend/BAL/VerifyAccessTokenManager.cs
@@ -0,0 +1,33 @@
+﻿using DAL;
+using Model.DTO;
+using Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAL
+{
+    public class VerifyAccessTokenManager : IDisposable
+    {
+        private IConfig _config;
+        public VerifyAccessTokenManager(IConfig config)
+        {
+            _config = config;
+        }
+
+        public void Dispose()
+        {
+            // throw new NotImplementedException();
+        }
+
+        public async Task<Response<dynamic>> VerifyAccessToken(access_tokenDto access_TokenDto)
+        {
+            using (VerifyAccessTokenDBHelper db = new VerifyAccessTokenDBHelper(_config))
+            {
+                return await db.VerifyAccessToken(access_TokenDto);
+            }
+        }
+    }
+}
